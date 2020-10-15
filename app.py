a26757506023f3bd2a0a1af853efe39de97ab7ed
rcuevass <rogercuevas.ca@gmail.com>
2020-10-15 11:47:27 -0400

    updated layout and renamed app file

--- app.py
+++ app.py
@@ -64,11 +64,9 @@ def search_results(urlsearch):
 
     summary = article.summary
 
-    return render_template("results.html", search_string = search_string, title = title, published_date=published_date, author = author, image = image, keyword = keyword, summary = summary)
+    return render_template("results.html", search_string = search_string, title = title, published_date=published_date,
+                           author = author, image = image, keyword = keyword, summary = summary)
 
 
-    # return render_template("results.html", search_string = search_string, data = data, title=title, published_date=published_date, author = author, image = image, cloud = cloud)
-
-    # return render_template("results.html", search_string = search_string, data = data, title=title, published_date=published_date, author = author, image = image, cloud = cloud, summary = summary)           
 if __name__ == '__main__':
-      app.run()
+    app.run()
