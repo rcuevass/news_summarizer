efff9c35f1e466928f8a4904aedc926fc6dbacce
rcuevass <rogercuevas.ca@gmail.com>
2020-11-25 18:34:26 -0500

    Adding README files to folders to better document code

--- forms.py
+++ forms.py
@@ -1,4 +1,5 @@
 from wtforms import Form, StringField
 
+
 class UrlSearchForm(Form):
     search = StringField('')
---
+++ templates/README.md
@@ -0,0 +1,3 @@
+# HTML templates
+
+Folder to store HTML templates used to render the app
